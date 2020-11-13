56dc7769fb572e1e5c56d3c2a16b4f77d218d237
ALDO <aldo@aldos-mbp.attlocal.net>
2020-11-12 21:50:54 -0800

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ textAlign: "center", color: "white" }}>
+      Made by ALDO GAMBOA
+    </div>
+  );
+}
+
+export default Footer;
